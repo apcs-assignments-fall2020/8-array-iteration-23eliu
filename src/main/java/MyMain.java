4eafad3f1c3fc2967b873e7e015868044c1ad2cb
23eliu <23eliu@athenian.org>
2020-09-29 01:37:41 +0000

    reverse finished

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,14 +2,27 @@ public class MyMain {
 
     // Reverses an array
     public static int[] reverse(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int[] reversed = new int[arr.length];
+        int count = 0;
+        for (int i = arr.length-1; i >= 0; i--) {
+            reversed[count] = arr[i];
+            count++;
+        }
+        return reversed;
     }
 
     // Finds the second largest number in an array
     public static int secondLargest(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        int big = arr[0];
+        int second = 0;
+        
+        for (int i = 1; i < arr.length-1; i++) {
+            if (arr[i] > big) {
+                second = big;
+                big = arr[i];
+            }
+        }
+        return second;
     }
 
     // Checks to see if an array contains a geometric series
